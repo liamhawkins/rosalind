6b0e4e2ca2f5aec35f94705ad65de6d3100020bb
Liam <liamhawkinscu@gmail.com>
2020-02-25 12:36:39 -0500

    Add challenge directions

--- bioinformatics_stronghold.py
+++ bioinformatics_stronghold.py
@@ -1,12 +1,25 @@
 def count_bases(s):
+    """
+    Given: A DNA string s of length at most 1000 nt.
+    Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G',
+    and 'T' occur in s.
+    """
     return s.count('A'), s.count('C'), s.count('G'), s.count('T')
 
 
-def translate(s):
-    return s.replace('T', 'U')
+def transcribe(t):
+    """
+    Given: A DNA string t having length at most 1000 nt.
+    Return: The transcribed RNA string of t.
+    """
+    return t.replace('T', 'U')
 
 
 def reverse_complement(s):
+    """
+    Given: A DNA string s of length at most 1000 bp.
+    Return: The reverse complement s^c of s.
+    """
     comp = {
         'A': 'T',
         'C': 'G',
@@ -17,6 +30,10 @@ def reverse_complement(s):
 
 
 def num_point_mutations(s, t):
+    """
+    Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+    Return: The Hamming distance dH(s,t).
+    """
     num = 0
     for x, y in zip(s, t):
         num += x != y
--- python_village.py
+++ python_village.py
@@ -3,23 +3,45 @@ from functools import reduce
 
 
 def installing_python():
+    """
+    After downloading and installing Python, type import this into the Python command line and see what happens.
+    Then, click the "Download dataset" button below and copy the Zen of Python into the space provided
+    """
     import this
 
 
 def variable_and_some_arithmetic(a, b):
+    """
+    Given: Two positive integers a and b, each less than 1000.
+    Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths
+    a and b.
+    """
     return a ** 2 + b ** 2
 
 
 def strings_and_lists(s, a, b, c, d):
+    """
+    Given: A string s of length at most 200 letters and four integers a, b, c and d.
+    Return: The slice of this string from indices a through b and c through d (with space in between), inclusively.
+    In other words, we should include elements s[b] and s[d] in our slice.
+    """
     return f'{s[a:b + 1]} {s[c:d + 1]}'
 
 
 def conditions_and_loops(a, b):
+    """
+    Given: Two positive integers a and b (a<b<10000).
+    Return: The sum of all odd integers from a through b, inclusively.
+    """
     odds = [x for x in range(a, b + 1) if x % 2 != 0]
     return reduce(lambda x, y: x+y, odds)
 
 
 def working_with_files(in_path, out_path):
+    """
+    Given: A file containing at most 1000 lines.
+    Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
+    """
     with open(in_path, 'r') as f:
         lines = f.readlines()
 
@@ -29,6 +51,11 @@ def working_with_files(in_path, out_path):
 
 
 def dictionaries(s):
+    """
+    Given: A string s of length at most 10000 letters.
+    Return: The number of occurrences of each word in s, where words are separated by spaces. Words are case-sensitive,
+    and the lines in the output can be in any order.
+    """
     for key, value in dict(Counter(s.split())).items():
         print(f'{key} {value}')
 
